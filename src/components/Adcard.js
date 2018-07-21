b089e9052543c36bb94f33af0213b912b132d5a3
hasanirshad <hasanirshad007@gmail.com>
2018-07-21 08:05:04 -0700

    changed adcard

--- src/components/Adcard.js
+++ src/components/Adcard.js
@@ -8,6 +8,7 @@ export default class Adcard extends React.Component{
         return(
             <div className="Adcard" >
                 <p>{this.props.status}</p>
+                <img src='../../images/img1.jpg'/>
             </div>
         );
     }
