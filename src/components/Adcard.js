574e32f2e48b691655966dad1ebb661d122818cd
hasanirshad <hasanirshad007@gmail.com>
2018-07-22 12:47:36 -0700

    image position is good now

--- src/components/Adcard.js
+++ src/components/Adcard.js
@@ -7,7 +7,7 @@ export default class Adcard extends React.Component{
     render(){
         return(
             <div className="Adcard" >
-                <p>{this.props.status}</p>
+                {/*<p>{this.props.status}</p>*/}
                 <img src='../../images/img1.jpg'/>
             </div>
         );
